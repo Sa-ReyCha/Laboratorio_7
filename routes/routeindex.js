61979552c6832c49be29f7106f3a785a43566be0
Sa-ReyCha <a01027358@tec.mx>
2023-02-04 22:52:06 -0600

    Cambio 02

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,11 +1,13 @@
 const { render } = require('ejs');
 const express = require('express');
 const router = express.Router(); //define lo que lo hace router
-//const Task = require('../model/task');
+const Task = require('../model/task'); // esto se trae el task original de model task
 
 
 router.get('/', async function(req,res){
-  res.render('index');
+  let tasks = await Task.find()
+  console.log(tasks);
+  res.render('index', {tasks});
 });
 
 
@@ -13,6 +15,17 @@ router.get('/newPost', async (req,res) =>{
   res.render('newPost');
 });
 
+//agregar nuevos blogs
+router .post('/newPost', async function(req,res){ // el async lo que hace es esperarse
+
+  console.log(req.body) //- - recibo title, author y postdata - - justo lo que quiero // name es imporante
+  let task = new Task(req.body) // esto lo que hace es poder generar un nuevo Task 
+  await task.save() // se espera a que termina el codigo
+
+  
+  res.redirect("/") //regresa a casa
+
+})
 
 
 module.exports = router; // esto importa a app
